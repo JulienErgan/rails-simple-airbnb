b5fc62ec98115f69de8319469931b8e94e51f7d6
Julien Ergan <julien.ergan.code@gmail.com>
2020-05-13 17:31:38 +0900

    Day 28 - Rails Advanced Routing - Challenge 3 - Rails Simple Airbnb - progress update

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,20 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+puts 'Cleaning the database...'
+Flat.destroy_all
+
+puts 'Creating flats...'
+
+20.times do
+  Flat.create!(
+    name: Faker::Company.name,
+    address: Faker::Address.full_address,
+    description:Faker::Company.catch_phrase,
+    price_per_night:rand(50.300),
+    number_of_guests: rand(0.10)
+  )
+end
+
+puts "Finished creating #{Flat.count} flats!"
